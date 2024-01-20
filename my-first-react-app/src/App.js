87eb126b8fc65620af3deee926d0f096fb45e205
Marcos Villarreal <marcosvilla444@gmail.com>
2024-01-20 14:09:02 -0600

    Project Updated: Props and Dynamic Content

--- my-first-react-app/src/App.js
+++ my-first-react-app/src/App.js
@@ -1,13 +1,12 @@
+import Todo from './components/todo';
+
 function App() {
   return (
     <div>
       <h1>My Todos</h1>
-      <div>
-        <h2>TITLE</h2>
-        <div>
-          <button>Delete</button>
-        </div>
-      </div>
+      <Todo text='Learn React'/>
+      <Todo text='Master React'/>
+      <Todo text='Explore the full React course'/>
     </div>
   );
 }
