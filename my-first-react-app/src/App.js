961cb3c00ec3952f44ea9ae2f8c2a4bd3b544d4c
Marcos Villarreal <marcosvilla444@gmail.com>
2024-01-22 20:40:47 -0600

    Project Updated w/ Props and Modal

--- my-first-react-app/src/App.js
+++ my-first-react-app/src/App.js
@@ -1,4 +1,6 @@
 import Todo from './components/todo';
+import Modal from './components/modal';
+import Backdrop from './components/backdrop';
 
 function App() {
   return (
@@ -7,6 +9,8 @@ function App() {
       <Todo text='Learn React'/>
       <Todo text='Master React'/>
       <Todo text='Explore the full React course'/>
+      <Modal />
+      <Backdrop />
     </div>
   );
 }
